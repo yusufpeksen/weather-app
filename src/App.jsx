caa3dd9f0fe768c9e9ce6ef9f539728c3fb3b475
Ahmed Yusuf Pekşen <ahmetpeksen77@gmail.com>
2023-10-15 17:36:32 +0300

    Next days forecast guesses added.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [country, setCountry] = useState("");
   const [degree, setDegree] = useState("");
   const [foreCast, setForeCast] = useState("");
+  const [nextForeCasts , setNextForeCasts] = useState([]);
   const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
@@ -18,13 +19,15 @@ function App() {
         )
         .then((response) => {
           const data = response.data;
+          const nextForecastDatas = (data.forecast.forecastday).slice(1,3)
           setImgWeather(data.current.condition.icon);
           setCountry(data.location.country);
           setDegree(data.current.feelslike_c);
           setForeCast(data.current.condition.text);
+          setNextForeCasts(nextForecastDatas)
         })
         .catch((err) => {
-          console.log("Belirtilen şehir bulunamadı.");
+          console.log("This city had not found.");
           setImgWeather(null);
           setCountry(null);
           setDegree(null);
@@ -32,6 +35,8 @@ function App() {
         });
     }
   }, [city]);
+
+  console.log(nextForeCasts)
 
   const handleInputChange = (e) => {
     const query = e.target.value;
@@ -43,7 +48,6 @@ function App() {
         )
         .then((response) => {
           setSearchResults(response.data);
-          console.log(response.data)
         })
         .catch((error) => {
           console.error(error);
@@ -60,13 +64,13 @@ function App() {
   };
 
   return (
-    <div className="bg-[#542274] w-[650px] h-[600px] rounded-[25px] flex items-center flex-col">
+    <div className="bg-[#542274] w-[750px] h-[700px] rounded-[25px] flex items-center flex-col">
       <div className="flex mt-[50px]">
         <div className="flex flex-col">
         <input
           ref={inputRef}
-          placeholder="Şehir Ara"
-          className="rounded-[10px] outline-none px-[15px] h-[30px] w-[250px]"
+          placeholder="Find City"
+          className="rounded-[10px] outline-none px-[15px] h-[40px] w-[300px]"
           onChange={handleInputChange}
         />
         <ul className="block h-[100px] overflow-y-auto">
@@ -74,7 +78,7 @@ function App() {
             <li
               key={location.id}
               onClick={() => handleResultClick(location)}
-              className="cursor-pointer text-white bg-purple-500 my-[3px] rounded-[10px] px-[8px]"
+              className="cursor-pointer text-white bg-purple-500 my-[3px] rounded-[10px] px-[10px] transition-colors duration-300 hover:bg-purple-300 flex items-center h-[30px]"
             >
               {location.name}
             </li>
@@ -83,12 +87,12 @@ function App() {
         </div>
         <button
           onClick={() => setCity(inputRef.current.value)}
-          className="bg-[#9639d9] h-[30px] rounded-[10px] px-[10px] py-[4px] ml-[8px] text-white text-center transition-colors duration-300 hover:bg-[#b570e7]"
+          className="bg-[#9639d9] h-[40px] rounded-[10px] px-[20px] py-[4px] ml-[8px] text-white text-center transition-colors duration-300 hover:bg-[#b570e7]"
         >
-          Ara
+          Find
         </button>
       </div>
-      <div className="mt-[50px] flex flex-col items-center">
+      <div className="flex flex-col items-center text-center">
         <img className="h-[150px]" src={imgWeather} />
         {foreCast && <h2 className="text-white text-[30px]">{foreCast}</h2>}
         {city && country && (
@@ -97,6 +101,18 @@ function App() {
           </h2>
         )}
         {degree && <h2 className="text-white text-[30px]">{degree} °C</h2>}
+      </div>
+      <div className="flex gap-52">
+        {
+          nextForeCasts.map((forecast) => (
+            <div className="flex flex-col items-center justify-center">
+              <p className="text-white">{forecast.date}</p>
+              <img className="" src={forecast.day.condition.icon}></img>
+              <h3 className="text-white text-[20px]">{forecast.day.condition.text}</h3>
+              <h3 className="text-white text-[20px]">AVG : {forecast.day.avgtemp_c} °C</h3>
+            </div>
+          ))
+        }
       </div>
     </div>
   );
