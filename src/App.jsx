a65c5effc04b566c0164952d8f7b6f8c8a97b806
Ahmed Yusuf Pekşen <ahmetpeksen77@gmail.com>
2023-10-15 17:05:28 +0300

    You can check every city in the world with search.

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,105 @@
-import axios from "axios"
-import { useRef, useState } from "react"
+import axios from "axios";
+import React, { useRef, useState, useEffect } from "react";
 
 function App() {
+  const inputRef = useRef("");
+  const [city, setCity] = useState("");
+  const [imgWeather, setImgWeather] = useState("");
+  const [country, setCountry] = useState("");
+  const [degree, setDegree] = useState("");
+  const [foreCast, setForeCast] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
 
-  const inputRef = useRef("")
+  useEffect(() => {
+    if (city) {
+      axios
+        .get(
+          `http://api.weatherapi.com/v1/forecast.json?key=6cf74598e9e0485d991182324231310&q=${city}&days=7`
+        )
+        .then((response) => {
+          const data = response.data;
+          setImgWeather(data.current.condition.icon);
+          setCountry(data.location.country);
+          setDegree(data.current.feelslike_c);
+          setForeCast(data.current.condition.text);
+        })
+        .catch((err) => {
+          console.log("Belirtilen şehir bulunamadı.");
+          setImgWeather(null);
+          setCountry(null);
+          setDegree(null);
+          setForeCast(null);
+        });
+    }
+  }, [city]);
 
-  const [city , setCity] = useState("")
-  const [imgWeather , setImgWeather] = useState("")
-  const [country , setCountry] = useState("")
-  const [degree , setDegree] = useState("")
-  const [foreCast , setForeCast] = useState("")
-  
+  const handleInputChange = (e) => {
+    const query = e.target.value;
 
-  axios.get(`http://api.weatherapi.com/v1/forecast.json?key=6cf74598e9e0485d991182324231310&q=${city}&days=7`)
-    .then(response => {
-      //console.log(response.data.forecast.forecastday[0])
-      setImgWeather(response.data.current.condition.icon)
-      setCountry(response.data.location.country)
-      setDegree(response.data.current.feelslike_c)
-      setForeCast(response.data.current.condition.text)
-    }).catch(err => {
-      console.log("Does not exist this city.")
-      setImgWeather(null)
-      setCountry(null)
-      setDegree(null)
-      setForeCast(null)
-    })
+    if (query.length >= 3) {
+      axios
+        .get(
+          `https://api.weatherapi.com/v1/search.json?key=6cf74598e9e0485d991182324231310&q=${query}`
+        )
+        .then((response) => {
+          setSearchResults(response.data);
+          console.log(response.data)
+        })
+        .catch((error) => {
+          console.error(error);
+          setSearchResults([]);
+        });
+    } else {
+      setSearchResults([]);
+    }
+  };
 
+  const handleResultClick = (location) => {
+    setCity(location.name);
+    setSearchResults([]);
+  };
 
   return (
-    <div className="bg-[#542274] w-[650px] h-[500px] rounded-[25px] flex items-center flex-col">
-      <div>
-        <input ref={inputRef} placeholder="Search City" className="mt-[50px] rounded-[10px] outline-none px-[15px] h-[30px] w-[250px]"></input>
-        <button onClick={() => setCity(inputRef.current.value)} className="bg-[#9639d9] rounded-[10px] px-[10px] py-[4px] ml-[8px] text-white text-center transition-colors duration-300 hover:bg-[#b570e7]">Search</button>
+    <div className="bg-[#542274] w-[650px] h-[600px] rounded-[25px] flex items-center flex-col">
+      <div className="flex mt-[50px]">
+        <div className="flex flex-col">
+        <input
+          ref={inputRef}
+          placeholder="Şehir Ara"
+          className="rounded-[10px] outline-none px-[15px] h-[30px] w-[250px]"
+          onChange={handleInputChange}
+        />
+        <ul className="block h-[100px] overflow-y-auto">
+          {searchResults.map((location) => (
+            <li
+              key={location.id}
+              onClick={() => handleResultClick(location)}
+              className="cursor-pointer text-white bg-purple-500 my-[3px] rounded-[10px] px-[8px]"
+            >
+              {location.name}
+            </li>
+          ))}
+        </ul>
+        </div>
+        <button
+          onClick={() => setCity(inputRef.current.value)}
+          className="bg-[#9639d9] h-[30px] rounded-[10px] px-[10px] py-[4px] ml-[8px] text-white text-center transition-colors duration-300 hover:bg-[#b570e7]"
+        >
+          Ara
+        </button>
       </div>
       <div className="mt-[50px] flex flex-col items-center">
-        <img className="h-[150px]" src={imgWeather}></img>
+        <img className="h-[150px]" src={imgWeather} />
         {foreCast && <h2 className="text-white text-[30px]">{foreCast}</h2>}
-        {city && country && <h2 className="text-white text-[30px]">{city.toUpperCase() + " / " + country}</h2>}
+        {city && country && (
+          <h2 className="text-white text-[30px]">
+            {city.toUpperCase()} / {country}
+          </h2>
+        )}
         {degree && <h2 className="text-white text-[30px]">{degree} °C</h2>}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
